cfe5ecdb0c2b9b495b49ce83f6c7650e139afac8
mikahimself <mika.laihanen@gmail.com>
2021-09-11 10:33:56 +0300

    Fine-tuning shadows

--- scripts/EnemyMotion.gd
+++ scripts/EnemyMotion.gd
@@ -5,8 +5,9 @@ var target_positions = []
 var path_attempt_max = 10
 
 func handle_input(event):
-	if event.is_action_pressed("simulate_damage"):
-		emit_signal("finished", "stagger")
+	#if event.is_action_pressed("simulate_damage"):
+	#	emit_signal("finished", "stagger")
+	pass
 
 func get_input_direction():
 	var input_direction = owner.navigation.get_simple_path(Vector2(10, 10), Vector2(200, 200))
